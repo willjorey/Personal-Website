0a25254542428be23b3efd74a28f2bc8417dd556
William <william.j.schober.reyes@gmail.com>
2019-02-04 17:07:24 -0500

    Added banner and profile images"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import profile from './assets/profile.jpg';
+import profile from './assets/0.jpeg';
+import banner from './assets/banner2.jpeg';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id='About'>          
+        <div id='About'>
+          <div id='banner' style={{height:'200px'}}>
+          <img src={banner} width='100%' style={{ borderRadius:'10px', border:'1px solid black'}}/>
+          </div>
+          <img src={profile} style={{borderRadius: '100px', border: '1px solid black', marginBottom: '50px'}}/>
           <h1 style={{fontWeight:'bold'}}>About Me</h1>
-          
+
           <p> I am an inovative graduate student from the University of Toronto who is ambitious and keen to learning new concepts and techniques.
             I have a set of skills in full-stack and mobile application devlopment with Android and IOS.
           </p>
@@ -22,20 +27,61 @@ class App extends Component {
           <br/>
           
           <h3 style={{fontWeight:'bold'}}>Publications</h3>
+          <hr/>
           <p>Reyes, W. (2018). Sphere Packing. Retrieved from <a href='https://www.mathinactionjournal.com/sphere-packing'>https://www.mathinactionjournal.com/sphere-packing</a></p>
 
           <h3 style={{fontWeight:'bold'}}>Education</h3>
+          <hr/>
           <p> Honours Bachelor of Science - Double Major in Mathematics and Statistics, University of Toronto</p>
 
-          <h2 style={{fontWeight:'bold'}}>Development</h2>
-          <hr/>
-          <p style={{fontSize: '15px'}}>* List of proficient skills with deeper focus in <b>bold</b></p>
-          <p><b>Javascript</b>, Java, Python,  AngularJS, <b>ReactJS</b>, <b>Redux</b>, <b>React Native</b>, Ionic, Cordova, <b>Node.js</b>, <b>Jest</b>,
-           Enzyme, <b>Firebase</b>, MongoDB, AWS, Bash, C, C#, SQL, MySQL, <b>GIT</b>, SVN, <b>HTML5</b>, <b>CSS</b>, LaTeX, MVC, <b>REST</b>,
-            Bootstrap, R, SAS</p>
+          <div>
+            <h2 style={{fontWeight:'bold'}}>Development</h2>
+            <hr/>
+            <p style={{fontSize: '15px'}}>* List of proficient skills with deeper focus in <b>bold</b></p>
+            <p><b>Javascript</b>, Java, Python,  AngularJS, <b>ReactJS</b>, <b>Redux</b>, <b>React Native</b>, <b>Expo</b>, Ionic, Cordova, <b>Node.js</b>, <b>Jest</b>,
+            Enzyme, <b>Firebase</b>, MongoDB, AWS, Bash, C, C#, SQL, MySQL, <b>GIT</b>, SVN, <b>HTML5</b>, <b>CSS</b>, LaTeX, MVC, <b>REST</b>,
+              Bootstrap, R, SAS</p>
+
+            <br/>
+            <h3 style={{fontWeight:'bold'}}>Work Experience</h3>
+            <p><b>Data Analyst</b> - Octacom Limited, Richmondhill, Ontario - April 2014 to September 2017</p>
+
+            <ul>
+              <li>Support document imaging production, analyzing and handling confidential documents</li>
+              <li>Quality check thousands of document images indexed by employees or scanned images</li>
+            </ul>
+
+            <p><b>Tutor</b> - York Region Tutoring, Markham, Ontario - October 2018 to Present</p>
+
+            <ul>
+              <li>Assist and teach high school students grade 9 - 12 mathematics or 1st year university mathematics </li>
+            </ul>
+          </div>
+
+          <div>
+            <h4 style={{fontWeight:'bold'}}>Projects</h4>
+            <ul>
+              <li><a href='https://github.com/willjorey/React-Native/tree/master/Login'>Sports Hybrid Mobile App</a> - React Native application used to
+              display sporting event information from local tournaments. Allows parents, players, coaches and employees to view scores, schedules, locations,
+              and statistics.
+              </li>
+
+              <li><a href='https://github.com/willjorey/React'>Sports App Web API</a> - User interface friendly api for admins to update or add tournament information
+              that will be displayed on the sports application.
+              </li>
+
+              <li><a href='https://github.com/willjorey/React-Native/tree/master/MoneyApp'>Money App</a> - A budget app that keeps track of how much money you spend.
+              </li>
+
+              
+            </ul>
+          </div>
 
           <br/>
-          <h3 style={{fontWeight:'bold'}}>Work Experience</h3>
+          <h3 style={{fontWeight:'bold'}}>Volunteer and Community Involvement</h3>
+          <hr/>
+          <p><b>Print Distribution Associate</b> - Toronto Pan American Games, Markham, Ontario - July 2015 to August 2015</p>
+          <p><b>Assistant Coach</b> - Markham Unionville Minor Basketball Association (MUMBA), Markham, Ontario - September 2011 to April 2015</p>
 
           <br/>
           <h3 style={{fontWeight:'bold',}}>Proud Achievements</h3>
@@ -46,15 +92,17 @@ class App extends Component {
             </li>
           
             <li>
-              In 2015 I volunteered in the Toronto PanAm Games as a <b>Print Distributor</b> and got to watch top athletes compete in water polo, table tennis, 
+              In 2015 I volunteered in the Toronto PanAm Games as a <b>Print Distribution Associate</b> and got to watch top athletes compete in water polo, table tennis, 
               and badminton. I even got to meet some of the Gold medal winners.
             </li>
           </ul>
 
-          <h3 style={{fontWeight:'bold'}}>Volunteer and Community Involvement</h3>
-          <hr/>
-          <p><b>Print Distribution Associate</b> - Toronto Pan American Games, Markham, Ontario - July 2015 to August 2015</p>
-          <p><b>Assistant Coach</b> - Markham Unionville Minor Basketball Association (MUMBA), Markham, Ontario - September 2011 to April 2015</p>
+          <h3 style={{fontWeight:'bold'}}>Contact</h3>
+          Email: <a href='william.j.schober.reyes@gmail.com'>william.j.schober.reyes@gmail.com</a>
+          <br/>
+          LinkedIn: <a href='https://www.linkedin.com/in/william-reyes-23016a81/'>linkedin.com/in/william-reyes-23016a81</a>
+          <br/>
+          Github: <a href='https://github.com/willjorey'>github.com/willjorey</a>
 
         </div>
 
